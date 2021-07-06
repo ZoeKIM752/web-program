4dfba0a2f550a8407e939514dd684d5014deea95
ZoeKIM752 <carpediem752@naver.com>
2021-07-06 16:54:04 +0900

    make pagination

--- DU/src/main/java/du/board/controller/BoardController.java
+++ DU/src/main/java/du/board/controller/BoardController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import du.board.domain.BoardVO;
 import du.board.service.BoardService;
+import du.common.Pagination;
 
 @Controller
 public class BoardController {
@@ -17,9 +19,19 @@ public class BoardController {
 	private BoardService boardService;
 	
 	@RequestMapping("/boardListPage.do")
-	public ModelAndView boardListPage() {
+	public ModelAndView boardListPage(
+			@RequestParam(required = false, defaultValue = "1") int page,
+			@RequestParam(required = false, defaultValue = "1") int range) {
+		
 		ModelAndView mav = new ModelAndView("board/boardList.jsp");
-		List<BoardVO> boardList = boardService.selectBoardList();
+		
+		int listCnt = boardService.selectBoardListCnt();
+		
+		Pagination pagination = new Pagination();
+		pagination.pageInfo(page, range, listCnt);
+		mav.addObject("pagination", pagination);
+		
+		List<BoardVO> boardList = boardService.selectBoardList(pagination);
 		mav.addObject("boardList", boardList);
 		
 		return mav;
